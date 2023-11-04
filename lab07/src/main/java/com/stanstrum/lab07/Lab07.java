342441c1ced0d6b567d7a806209305d88b27871d
Stan Strum <stanleystrum@gmail.com>
2023-11-04 14:04:58 -0700

    Add in-progress lab 07

---
+++ lab07/src/main/java/com/stanstrum/lab07/Lab07.java
@@ -0,0 +1,32 @@
+/* Author: Stan Strum (555-010-9379)
+ * Date: 3 Nov 2023
+ * Description: Lab 7: Rainfall
+ */
+
+package com.stanstrum.lab07;
+
+/**
+ * @author Stan Strum <stanleystrum@gmail.com>
+ */
+public class Lab07 {
+	public static class Rainfall {
+		public static enum Month {
+			January,
+			February,
+			March,
+			April,
+			May,
+			June,
+			July,
+			August,
+			September,
+			October,
+			November,
+			December;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
