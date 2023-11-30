64f01c213eb02296192ae9e36521f41463834809
Stan Strum <stanleystrum@gmail.com>
2023-11-29 21:32:27 -0800

    project03: add base files

---
+++ project03/src/main/java/com/stanstrum/project03/Project03.java
@@ -0,0 +1,14 @@
+/* Author: Stan Strum (555-010-9379)
+ * Date: 29 Nov 2023
+ * Description: Project 3: Flight Itinerary Processor
+ */
+package com.stanstrum.project03;
+
+/**
+ * @author Stan Strum {@literal <stanleystrum@gmail.com>}
+ */
+public class Project03 {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ project03/src/main/java/com/stanstrum/project03/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Project 3: A flight itinerary processor that takes into accound
+ * distance between airports and compiles travel information
+ * into an HTML file.
+ */
+package com.stanstrum.project03;
