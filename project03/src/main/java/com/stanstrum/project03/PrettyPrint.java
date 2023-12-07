6f052ffe6e68b6c0689acfa7d9cbeb8c614d19e4
Stan Strum <stanleystrum@gmail.com>
2023-12-07 03:52:53 -0800

    project03: copy over time-formatting code from project01

--- project03/src/main/java/com/stanstrum/project03/PrettyPrint.java
+++ project03/src/main/java/com/stanstrum/project03/PrettyPrint.java
@@ -77,4 +77,80 @@ public class PrettyPrint {
 		// Now reverse the characters.
 		return reverse_string(backwards_string);
 	}
+
+	public static final int SECONDS_PER_MINUTE = 60;
+	public static final int SECONDS_PER_HOUR = 60 * SECONDS_PER_MINUTE;
+	public static final int SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR;
+
+	public static String prettyPrintTime(int seconds) {
+		// Get rounded-down amount of days by dividing by seconds.
+		int days = seconds / SECONDS_PER_DAY;
+
+		// Remove the amount of time we just printed.
+		seconds %= SECONDS_PER_DAY;
+
+		// Rinse & repeat
+		int hours = seconds / SECONDS_PER_HOUR;
+		seconds %= SECONDS_PER_HOUR;
+
+		int minutes = seconds / SECONDS_PER_MINUTE;
+		seconds %= SECONDS_PER_MINUTE;
+
+		// Marker for if there is any preceding text needing a comma separator.
+		boolean did_print_any_time = false;
+
+		if (days != 0) {
+			// "day" if only 1, "days" otherwise.
+			String pluralized = PrettyPrint.pluralize("day", days);
+
+			// Prints "5 days", for example.
+			System.out.printf("%d %s", days, pluralized);
+
+			// Indicate that something has been printed.
+			did_print_any_time = true;
+		}
+
+		// Repeat for hours.
+		if (hours != 0) {
+			if (did_print_any_time) {
+				System.out.print(", ");
+			}
+
+			String pluralized = PrettyPrint.pluralize("hour", hours);
+
+			System.out.printf("%d %s", hours, pluralized);
+
+			did_print_any_time = true;
+		}
+
+		// Again for minutes.
+		if (minutes != 0) {
+			if (did_print_any_time) {
+				System.out.print(", ");
+			}
+
+			String pluralized = PrettyPrint.pluralize("minute", minutes);
+
+			System.out.printf("%d %s", minutes, pluralized);
+
+			did_print_any_time = true;
+		}
+
+		// Seconds are handled differently.  They will show up if
+		// they are non-zero OR if no other units were printed.
+		// However, whether to print the leading comma delimiter
+		// depends on if anything has been printed yet.
+		if (did_print_any_time && seconds > 0) {
+			System.out.print(", ");
+		}
+
+		// Seconds shows up if no time was printed or if the seconds are nonzero.
+		if (!did_print_any_time || seconds > 0) {
+			String pluralized = PrettyPrint.pluralize("second", seconds);
+			System.out.printf("%d %s", seconds, pluralized);
+		}
+
+		// Ending period and newline.
+		System.out.println(".");
+	}
 }
