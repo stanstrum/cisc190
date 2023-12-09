39c9ae6ba0d242f7280c835de7abf149e704993b
Stan Strum <stanleystrum@gmail.com>
2023-12-08 16:52:07 -0800

    project03: whitespace

--- project03/src/main/java/com/stanstrum/project03/Utils/LegAirportQuerier.java
+++ project03/src/main/java/com/stanstrum/project03/Utils/LegAirportQuerier.java
@@ -5,37 +5,37 @@ import java.util.Scanner;
 import com.stanstrum.project03.FlightInfo.Airport;
 
 public abstract class LegAirportQuerier extends PreprocessedQuerier<String, Airport> {
-    private int legNum;
+  private int legNum;
 
-    public final void setLegNum(int legNum) {
-      this.legNum = legNum;
-    }
-
-    protected final int getLegNum() {
-      return this.legNum;
-    }
-
-    public LegAirportQuerier(Scanner scanner) {
-      super(scanner);
-    }
-
-    @Override
-    protected final String scanValue() {
-      return this.scanner.nextLine();
-    }
-
-    @Override
-    protected final boolean verify(String code) {
-      return Resources.hasAirport(code);
-    }
-
-    @Override
-    protected final void showError(String code) {
-      System.out.println("Unknown airport code: " + code.toUpperCase());
-    }
-
-    @Override
-    protected final Airport preprocess(String code) {
-      return Resources.getAirport(code);
-    }
+  public final void setLegNum(int legNum) {
+    this.legNum = legNum;
   }
+
+  protected final int getLegNum() {
+    return this.legNum;
+  }
+
+  public LegAirportQuerier(Scanner scanner) {
+    super(scanner);
+  }
+
+  @Override
+  protected final String scanValue() {
+    return this.scanner.nextLine();
+  }
+
+  @Override
+  protected final boolean verify(String code) {
+    return Resources.hasAirport(code);
+  }
+
+  @Override
+  protected final void showError(String code) {
+    System.out.println("Unknown airport code: " + code.toUpperCase());
+  }
+
+  @Override
+  protected final Airport preprocess(String code) {
+    return Resources.getAirport(code);
+  }
+}
