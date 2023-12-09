a6d6cd85a21c6a177c67b804e846dfcb6013d3ef
Stan Strum <stanleystrum@gmail.com>
2023-12-08 16:35:47 -0800

    project03: add a specific LegAirportQuerier class to convert IATA codes to Airport instances

---
+++ project03/src/main/java/com/stanstrum/project03/Utils/LegAirportQuerier.java
@@ -0,0 +1,42 @@
+package com.stanstrum.project03.Utils;
+
+import java.util.Scanner;
+
+import com.stanstrum.project03.Airport;
+import com.stanstrum.project03.Resources;
+
+public abstract class LegAirportQuerier extends PreprocessedQuerier<String, Airport> {
+    private int legNum;
+
+    public final void setLegNum(int legNum) {
+      this.legNum = legNum;
+    }
+
+    protected final int getLegNum() {
+      return this.legNum;
+    }
+
+    public LegAirportQuerier(Scanner scanner) {
+      super(scanner);
+    }
+
+    @Override
+    protected final String scanValue() {
+      return this.scanner.nextLine();
+    }
+
+    @Override
+    protected final boolean verify(String code) {
+      return Resources.hasAirport(code);
+    }
+
+    @Override
+    protected final void showError(String code) {
+      System.out.println("Unknown airport code: " + code.toUpperCase());
+    }
+
+    @Override
+    protected final Airport preprocess(String code) {
+      return Resources.getAirport(code);
+    }
+  }
