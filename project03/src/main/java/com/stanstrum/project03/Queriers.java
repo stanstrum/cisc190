56914c2d89b0e1baad53c765cb9f69e883fc0622
Stan Strum <stanleystrum@gmail.com>
2023-12-08 20:02:14 -0800

    project03: add a LegQuerier base class for a SpeedQuerier implementation

--- project03/src/main/java/com/stanstrum/project03/Queriers.java
+++ project03/src/main/java/com/stanstrum/project03/Queriers.java
@@ -2,8 +2,9 @@ package com.stanstrum.project03;
 
 import java.util.Scanner;
 
-import com.stanstrum.project03.Utils.Querier;
 import com.stanstrum.project03.Utils.LegAirportQuerier;
+import com.stanstrum.project03.Utils.LegQuerier;
+import com.stanstrum.project03.Utils.Querier;
 
 public class Queriers {
   public static class LegsQuerier extends Querier<Integer> {
@@ -54,6 +55,41 @@ public class Queriers {
         "Enter the identifier for the departure airport (e.g. SAN) for leg %d: ",
         this.getLegNum()
       );
+    }
+  }
+
+  public static class LegSpeedQuerier extends LegQuerier<Double, Double> {
+    public LegSpeedQuerier(Scanner scanner) {
+      super(scanner);
+    }
+
+    @Override
+    protected void showPrompt() {
+      System.out.printf("Enter the speed for leg %d: ", this.getLegNum());
+    }
+
+    @Override
+    protected Double scanValue() throws Exception {
+      String line = this.scanner.nextLine();
+
+      return Double.parseDouble(line);
+    }
+
+    @Override
+    protected boolean verify(Double value) {
+      // Must be greater than zero since the formula for
+      // duration is distance / speed.  If speed is zero,
+      // duration is infinity.
+      return value > 0;
+    }
+
+    // We hae to implement this since our superclass
+    // uses the base PreprocessedQuerier, and the LegAirportQuerier
+    // needs the preprocess method.
+    @Override
+    protected Double preprocess(Double value) {
+      // Do nothing.
+      return value;
     }
   }
 }
